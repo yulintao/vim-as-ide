a9b8a34e92ee660224798f17fc32bb8d40a1a71b
yult <yult@qssec.com>
2019-04-24 14:30:34 +0800

    添加工作环境配置，添加tmux

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-sudo apt install autojump zsh
+sudo apt install tmux autojump zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
