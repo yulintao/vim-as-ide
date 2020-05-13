054ce9b295db93e3edb0863f7c4d7281a57b39f5
yulintao <oopsadm@gmail.com>
2020-05-13 15:37:46 +0800

    emacs: 修改emacs主题

--- emacs/init.el
+++ emacs/init.el
@@ -82,3 +82,16 @@
 ;;(ido-mode -1) ;; Turn off ido mode in case I enabled it accidentally
 ;;
 
+;; 改变Emacs主题
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (adwaita))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
