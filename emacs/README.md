6f37d2189958455eae2037e67df48039f68493dd
yulintao <oopsadm@gmail.com>
2020-05-13 16:58:57 +0800

    emacs: 修改emacs主题

--- emacs/README.md
+++ emacs/README.md
@@ -1,6 +1,9 @@
 # 重新载入配置文件
 
 init.el 需要放到~/.emacs.d/ 目录下
+
+quote 相当于 ' (quote foo) == 'foo
+
 M-x load-file  两次回车
 M-x eval-buffer 执行缓冲区所有Lisp命令
 C-x C-e 执行某一行Lisp代码
--- emacs/init.el
+++ emacs/init.el
@@ -7,6 +7,36 @@
 ;; common lisp externsion
 (require 'cl)
 
+;; add my packages
+(defvar my/packages '(
+	;; --- Auto-completion --
+	company
+	;; -- Better Editor ---
+	hungry-delete
+	swiper
+	counsel
+	smartparens
+	;; --- Themes ---
+	monokai-theme
+	;; solarized-theme
+	) "Default packages")
+
+(setq package-selected-packages my/packages)
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+(add-to-list 'my/packages 'monokai-theme)
+(load-theme 'monokai 1)
+
 ;; display line num
 (global-linum-mode 1)
 (setq linum-format "%d| ")
@@ -36,6 +66,12 @@
 ;; window config close toolbar（工具栏）
 (tool-bar-mode -1)
 
+;; 默认全屏
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; 当前行高亮
+(global-hl-line-mode 1)
+
 ;; 关闭文件滑动控件
 ;;(scrool-bar-mod -1)
 
@@ -54,47 +90,16 @@
 ;; display time at modeline
 ;; (display-time-mode 1)
 
-;; helm package
-;;(use-package helm
-;;  :diminish helm-mode
-;;  :init
-;;  (progn
-;;    (require 'helm-config)
-;;    (setq helm-candidate-number-limit 100)
-;;    ;; From https://gist.github.com/antifuchs/9238468
-;;    (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
-;;          helm-input-idle-delay 0.01  ; this actually updates things
-;;                                        ; reeeelatively quickly.
-;;          helm-yas-display-key-on-candidate t
-;;          helm-quick-update t
-;;          helm-M-x-requires-pattern nil
-;;          helm-ff-skip-boring-files t)
-;;    (helm-mode))
-;;  :bind (("C-c h" . helm-mini)
-;;         ("C-h a" . helm-apropos)
-;;         ("C-x C-b" . helm-buffers-list)
-;;         ("C-x b" . helm-buffers-list)
-;;         ("M-y" . helm-show-kill-ring)
-;;         ("M-x" . helm-M-x)
-;;         ("C-x c o" . helm-occur)
-;;         ("C-x c s" . helm-swoop)
-;;         ("C-x c y" . helm-yas-complete)
-;;         ("C-x c Y" . helm-yas-create-snippet-on-region)
-;;         ("C-x c b" . my/helm-do-grep-book-notes)
-;;         ("C-x c SPC" . helm-all-mark-rings)))
-;;(ido-mode -1) ;; Turn off ido mode in case I enabled it accidentally
-;;
-
-;; 改变Emacs主题
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (adwaita))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;;; 改变Emacs主题
+;;(custom-set-variables
+;; ;; custom-set-variables was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; '(custom-enabled-themes (quote (adwaita))))
+;;(custom-set-faces
+;; ;; custom-set-faces was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; )
